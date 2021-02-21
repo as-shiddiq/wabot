198dda4afafba503fd0b809882ebcbf1312fafd5
as-shiddiq <n.shiddiq@gmail.com>
2021-02-21 11:48:58 +0800

    coba push wa web

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const fs = require('fs');
+const { Client } = require('whatsapp-web.js');
+
+const client = new Client();
+const qrCode = require('qrcode');
+
 var express = require('express');
 var app = express();
 
@@ -5,7 +11,20 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  client.on('qr', async (qr) => {
+     // Generate and scan this code with your phone
+     var gambar = await qrCode.toDataURL(qr);
+     // console.log(gambar);
+    var body = "<center><img src='" + gambar + "'></img><br/>Silahkan scan qrcode ini dengan Whatsapp</center>";
+    response.send(body);
+  });
+
+  client.on('ready', () => {
+       var body = "<center><h1>Berhasil terhubung</h1></center>";
+       response.send(body);
+       console.log('Client is ready!');
+  });
+
 });
 app.use(function (req, res, next) {
 
@@ -25,6 +44,15 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
+
+
+client.on('message', message => {
+ if(message.body === 'ping') {
+   message.reply('sianjay');
+ }
+});
+
+client.initialize();
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
