bb123679fe9fde7b14e0ca55cc8c43263c624227
as-shiddiq <n.shiddiq@gmail.com>
2022-05-13 00:27:24 +0800

    push new

--- index.js
+++ index.js
@@ -1,40 +1,80 @@
+const express = require('express')
+const fetch = require('node-fetch');
 const fs = require('fs');
-var express = require('express');
-var app = express();
+const { Client, LocalAuth  } = require('whatsapp-web.js');
+let qrCode = require('qrcode');
+const app = express()
+const port = 5000
+let client = [];
+let apiUrl = 'http://wacoba.kodingakan.id/';
 
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/html',
+        'Pesan-Header': 'Scan QR'
+    });
+    let userNum = req.query.userNum;
+    if(userNum==undefined)
+    {
+        res.send('Error');
+    }
+    else
+    {
+        if(req.query.reset=='true')
+        {
+            console.log('reset session-'+userNum);
+            fs.rmSync('.wwebjs_auth/session-'+userNum, { recursive: true, force: true });
+        }
+        console.log(userNum);
+        client[userNum] = new Client({ 
+                            authStrategy: new LocalAuth ({clientId: userNum}),
+                            puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+                    });
 
-app.get('/', function(req, res) {
-   let body = `Hi!`;
-   res.send(body);
+        client[userNum].initialize();
+        client[userNum].on('qr', async (qr) => {
+            console.log(qr);
+            let gambar = await qrCode.toDataURL(qr);
+            let body = `<center><img src="${gambar}"></img><br/>Silahkan scan qrcode ini dengan Whatsapp</center>`;
+            res.write(body);
+        });
 
-});
+        client[userNum].on('authenticated', () => {
+            console.log(`AUTHENTICATED FOR ${userNum}`);
+            res.write('Connected');
+        });
 
-require("./app/routes.js")(app);
+        client[userNum].on('auth_failure', msg => {
+            // Fired if session restore was unsuccessful
+            console.error('AUTHENTICATION FAILURE', msg);
+        });
 
-app.use(function (req, res, next) {
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    // Pass to next layer of middleware
-    next();
-});
+        client[userNum].on('ready', async () => {
+            console.log('READY');
+            setInterval(async () =>{
+                let get = await fetch(`${apiUrl}list.php?akun=${userNum}`);
+                let resp = await get.json();
+                console.log(resp);
+                if(resp.status=='success')
+                {
+                    let data = resp.data;
+                    client[userNum].sendMessage(data.dest+'@c.us', data.text);
+                    //delete 
+                    let del = await fetch(`${apiUrl}index.php?delete=${data.name}&akun=${userNum}`);
+                    let respDel = await del.json();
+                    console.log(respDel);
+                }
+            },5000);
+        });
+
+        client[userNum].on('message', async (msg) => {
+            console.log(msg);
+        });
+    }
+
+})
 
 
-// client.on('message', message => {
-//  if(message.body === 'ping') {
-//    message.reply('sianjay');
-//  }
-// });
-
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(port, () => {
+  console.log(`Node app is running on port ${port}`);
 });
